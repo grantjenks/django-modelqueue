55a6eb34d5c1f473d851619eb76c427f5737905f
Grant Jenks <contact@grantjenks.com>
2018-03-29 12:55:36 -0700

    Close cursors for pypy

--- tests/www/www/__init__.py
+++ tests/www/www/__init__.py
@@ -4,8 +4,9 @@ from django.db.backends.signals import connection_created
 def improve_sqlite_performance(sender, connection, **kwargs):
     "Improve SQLite performance."
     if connection.vendor == 'sqlite':
-        cursor = connection.cursor()
-        cursor.execute('PRAGMA journal_mode = WAL')
-        cursor.execute('PRAGMA synchronous = NORMAL')
+        with connection.cursor() as cursor:
+            cursor.execute('PRAGMA journal_mode = WAL')
+        with connection.cursor() as cursor:
+            cursor.execute('PRAGMA synchronous = NORMAL')
 
 connection_created.connect(improve_sqlite_performance)
