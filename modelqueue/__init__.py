e8d8cb97324b307d9ba3b37019699c6a6478698e
Grant Jenks <contact@grantjenks.com>
2018-03-30 14:04:01 -0700

    Bump version to 0.0.6

--- modelqueue/__init__.py
+++ modelqueue/__init__.py
@@ -247,8 +247,8 @@ def run(queryset, field, action, retry=3, timeout=ONE_HOUR, delay=ZERO_SECS):
 
 
 __title__ = 'modelqueue'
-__version__ = '0.0.5'
-__build__ = 0x000005
+__version__ = '0.0.6'
+__build__ = 0x000006
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018 Grant Jenks'
