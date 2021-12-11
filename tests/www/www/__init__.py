8ad2401849966aae4eacb330d4b24af09987a707
Grant Jenks <gjenks@linkedin.com>
2021-12-10 23:07:55 -0800

    Run blue

--- tests/www/www/__init__.py
+++ tests/www/www/__init__.py
@@ -2,11 +2,12 @@ from django.db.backends.signals import connection_created
 
 
 def improve_sqlite_performance(sender, connection, **kwargs):
-    "Improve SQLite performance."
+    """Improve SQLite performance."""
     if connection.vendor == 'sqlite':
         with connection.cursor() as cursor:
             cursor.execute('PRAGMA journal_mode = WAL')
         with connection.cursor() as cursor:
             cursor.execute('PRAGMA synchronous = NORMAL')
+
 
 connection_created.connect(improve_sqlite_performance)
--- tests/www/www/admin.py
+++ tests/www/www/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 from .models import Task
 
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_filter = [
