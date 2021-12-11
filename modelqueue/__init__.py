3c2790b7e8ba82760d6223265590b49d497dab49
Grant Jenks <gjenks@linkedin.com>
2021-12-10 23:20:13 -0800

    Update pylint

--- modelqueue/__init__.py
+++ modelqueue/__init__.py
@@ -340,7 +340,7 @@ class Retry(Exception):
     Retry does *not* increment the attempt count of the task.
 
     """
-
+    # pylint: disable=super-init-not-called
     def __init__(self, delay=None):
         self.delay = delay
 
@@ -352,7 +352,7 @@ class Abort(Exception):
     is reached then the task will be canceled.
 
     """
-
+    # pylint: disable=super-init-not-called
     def __init__(self, delay=None):
         self.delay = delay
 
